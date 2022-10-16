322f362a3d6a13a187aff196d5596a05c9d9271a
Chan-Young Lee <chan9908181@gmail.com>
2022-10-16 18:20:01 +0200

    add the TODOs

--- src/homework1/homework1.java
+++ src/homework1/homework1.java
@@ -4,6 +4,6 @@ import static homework1.MiniJava.*;
 
 public class homework1 {
     public static void main(String[] args) {
-
+        //TODO program the right way to have the same out put as Chan.
     }
 }
