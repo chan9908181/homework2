e8b806a6f1706bdf6accf0a8d82812dfe361b958
corinnie <corinne.h.jung@gmail.com>
2022-10-16 21:43:57 +0200

    homework1 push

--- src/homework1/Homework1.java
+++ src/homework1/Homework1.java
@@ -5,5 +5,7 @@ import static homework1.MiniJava.*;
 public class Homework1 {
     public static void main(String[] args) {
         //TODO program the right way to have the same out put as Chan.
+        write(42);
+
     }
 }
